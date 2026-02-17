c00ea34f668d9a1b9ab8963b4ab1ab7c3135807a
CubedRhythm <80733322+CubedRhythm@users.noreply.github.com>
2026-02-17 13:21:27 -0800

    Add auto path with choreo event trigger for intake deploy

--- src/main/java/com/team9470/commands/Autos.java
+++ src/main/java/com/team9470/commands/Autos.java
@@ -3,13 +3,16 @@ package com.team9470.commands;
 import choreo.auto.AutoFactory;
 import choreo.auto.AutoRoutine;
 import choreo.auto.AutoTrajectory;
+import com.team9470.subsystems.shooter.Shooter;
 import com.team9470.subsystems.swerve.Swerve;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 public class Autos {
   private final AutoFactory m_autoFactory;
+  private final Shooter m_shooter;
 
-  public Autos(Swerve swerve) {
+  public Autos(Swerve swerve, Shooter shooter) {
+    m_shooter = shooter;
     m_autoFactory = swerve.createAutoFactory((sample, isStart) -> {
     });
   }
